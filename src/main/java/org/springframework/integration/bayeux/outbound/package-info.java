91937fd12b2793316ff618bcadbcd41fcc49e9a9
fabito <fabio.uechi@gmail.com>
2013-03-17 16:39:49 -0300

    creating pacakage for outbound adapter

---
+++ src/main/java/org/springframework/integration/bayeux/outbound/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides Bayeux outbound Messagehandler implementations.These components extend
+ * {@link org.springframework.integration.xmpp.core.AbstractBayeuxClientAwareMessageHandler}
+ */
+package org.springframework.integration.bayeux.outbound;
